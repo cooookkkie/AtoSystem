ad988621a383287be8e85debb91ccb3fc6a813b2
cooookkkie <aptl5620@gmail.com>
2024-08-08 13:42:19 +0900

    2024-08-08

---
+++ Libs/Tools/MySqlCommand.cs
@@ -0,0 +1,16 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Libs.Tools
+{
+    internal class MySqlCommand
+    {
+        private string v;
+        private MySqlConnection connection;
+
+        public MySqlCommand(string v, MySqlConnection connection)
+        {
+            this.v = v;
+            this.connection = connection;
+        }
+    }
+}
